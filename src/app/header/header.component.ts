c2f11f42cd6641ffbcb1962f81b8ccf10a2256a3
Mohit Kumar Sharma <mohits.sharma2@gmail.com>
2025-05-25 23:53:47 +0530

    Added Cards in landing page

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import {MatToolbar} from '@angular/material/toolbar';
 import {MatIcon} from '@angular/material/icon';
+import {MatButton} from '@angular/material/button';
 
 @Component({
   selector: 'app-header',
   imports: [
     MatToolbar,
-    MatIcon
+    MatIcon,
+    MatButton
   ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
--- src/app/landing/landing.component.ts
+++ src/app/landing/landing.component.ts
@@ -1,8 +1,27 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {MatButton} from '@angular/material/button';
+import {
+  MatCard,
+  MatCardActions,
+  MatCardContent,
+  MatCardHeader,
+  MatCardSubtitle,
+  MatCardTitle
+} from '@angular/material/card';
 
 @Component({
   selector: 'app-landing',
-  imports: [],
+  imports: [
+    CommonModule,
+    MatButton,
+    MatCardActions,
+    MatCardContent,
+    MatCardSubtitle,
+    MatCardTitle,
+    MatCardHeader,
+    MatCard
+  ],
   templateUrl: './landing.component.html',
   styleUrl: './landing.component.scss'
 })
