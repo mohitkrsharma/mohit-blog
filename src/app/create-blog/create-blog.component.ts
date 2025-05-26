4ac48b30a59e418cd915061b93bdd627e83c1f95
Mohit Kumar Sharma <mohits.sharma2@gmail.com>
2025-05-26 16:42:00 +0530

    Added Create blog UI

--- src/app/create-blog/create-blog.component.ts
+++ src/app/create-blog/create-blog.component.ts
@@ -1,14 +1,19 @@
 import { Component } from '@angular/core';
 import {MatLabel} from '@angular/material/form-field';
+import {FormsModule} from '@angular/forms';
+import {MatButton} from '@angular/material/button';
 
 @Component({
   selector: 'app-create-blog',
   imports: [
-    MatLabel
+    FormsModule,
+    MatButton
   ],
   templateUrl: './create-blog.component.html',
   styleUrl: './create-blog.component.scss'
 })
 export class CreateBlogComponent {
+  title: any;
+  content: any;
 
 }
