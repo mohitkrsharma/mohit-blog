ec1483b1efb2337c753b6b08e628332b9434c871
Mohit Kumar Sharma <mohits.sharma2@gmail.com>
2025-08-21 20:49:05 +0530

    implemented social media handles

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,11 +1,32 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {MatTooltip} from '@angular/material/tooltip';
 
 @Component({
   selector: 'app-footer',
-  imports: [],
+  imports: [
+    MatTooltip
+  ],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss'
 })
-export class FooterComponent {
+export class FooterComponent implements OnInit{
+  constructor() {
+  }
 
+  ngOnInit(): void {
+  }
+
+  openSocialMedia(platform: string): void {
+    const urls: { [key: string]: string } = {
+      facebook: 'https://www.facebook.com/mohit.sharma.921',
+      twitter: 'https://www.x.com/MohitSharma_PMY',
+      instagram: 'https://www.instagram.com/mohitsharma_mp',
+      linkedin: 'https://www.linkedin.com/in/mohitkumarsharma1/'
+    };
+
+    const url = urls[platform as keyof typeof urls];
+    if (url) {
+      window.open(url, '_blank');
+    }
+  }
 }
