c64abe47dd31f944253bd328b9f65de81ff4c482
Mohit Kumar Sharma <mohits.sharma2@gmail.com>
2025-08-28 13:32:59 +0530

    Removed commented code

--- src/app/pipes/paragraph.pipe.ts
+++ src/app/pipes/paragraph.pipe.ts
@@ -4,10 +4,6 @@ import { Pipe, PipeTransform } from '@angular/core';
   name: 'paragraph'
 })
 export class ParagraphPipe implements PipeTransform {
-
-/*  transform(value: string): string {
-    return value ? value.replace(/\n/g, '<br>') : '';
-  }*/
   transform(value: string): string {
     return value ? value.replace(/\n/g, '<br>') : '';
   }
